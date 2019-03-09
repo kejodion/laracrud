089826001ef04a6fc6aba53eea7dab866aed534f
kjjdion <kjjdion@gmail.com>
2019-03-09 01:15:02 -0500

    progress

--- config/laracrud.php
+++ config/laracrud.php
@@ -2,6 +2,7 @@
 
 return [
 
+    // available user roles
     'roles' => ['Admin'],
 
 ];
--- database/migrations/2019_03_09_054640_create_laracrud_columns.php
+++ database/migrations/2019_03_09_054640_create_laracrud_columns.php
@@ -1,34 +1,37 @@
 <?php
 
+use App\User;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
 class CreateLaracrudColumns extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
+        // add columns to users table
         Schema::table('users', function (Blueprint $table) {
             $table->string('role')->default(config('laracrud.roles.0'));
             $table->string('timezone')->default(config('app.timezone'));
         });
+
+        // create default admin user
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => bcrypt('admin123'),
+        ]);
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
+        // remove columns from users table
         Schema::table('users', function (Blueprint $table) {
             $table->dropColumn('role');
             $table->dropColumn('timezone');
         });
+
+        // delete default admin user
+        User::where('email', 'admin@example.com')->delete();
     }
 }
