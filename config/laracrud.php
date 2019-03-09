12eac40bd9c9e9f18b24be37bde7675bfa322c06
kjjdion <kjjdion@gmail.com>
2019-03-09 02:34:41 -0500

    progress

--- config/laracrud.php
+++ config/laracrud.php
@@ -5,4 +5,16 @@ return [
     // available user roles
     'roles' => ['Admin'],
 
+    // controllers used by package
+    'controllers' => [
+        'admin' => [
+            'home' => 'Kjjdion\Laracrud\Http\Controllers\Admin\HomeController',
+            'user' => 'Kjjdion\Laracrud\Http\Controllers\Admin\UserController',
+        ],
+        'auth' => [
+            'login' => 'Kjjdion\Laracrud\Http\Controllers\Auth\LoginController',
+            'profile' => 'Kjjdion\Laracrud\Http\Controllers\Auth\ProfileController',
+        ],
+    ],
+
 ];
