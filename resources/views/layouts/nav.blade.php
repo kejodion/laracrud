261bcc17967a883c23cb9c34f72a4f8bb1fb1758
kjjdion <kjjdion@gmail.com>
2019-03-10 01:45:50 -0500

    fix user active

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,5 +1,5 @@
 @can('Admin')
-    <li class="nav-item{!! request()->is('admin/users') ? ' class="active"' : '' !!}">
+    <li class="nav-item{!! request()->is('admin/users') ? ' active' : '' !!}">
         <a href="{{ route('admin.users') }}" class="nav-link">Users</a>
     </li>
 @endcan
